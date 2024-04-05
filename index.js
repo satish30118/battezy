48b0e7b1af4d27f9489bf17dcae0d1e9533d0042
Satish Maurya <satishmaurya30118@gmail.com>
2024-04-06 00:47:25 +0530

    fixes

--- index.js
+++ index.js
@@ -14,3 +14,16 @@ function showSlides() {
   slides[slideIndex - 1].style.display = "block";
   setTimeout(showSlides, 3000); // Change image every 2 seconds
 }
+
+
+const navbar = document.getElementById('navbar');
+
+function navbg (){
+  if(window.scrollY > 100){
+    navbar.style.background = "blue";
+  }else{
+    navbar.style.background = "transparent";
+  }
+}
+
+navbg();
